8872bdec140905e0a88d375e3d6c5c6bd9feff61
PavelPakhnutow <pavelpakhnutow@gmail.com>
2019-12-24 03:03:20 +0300

    Add class

---
+++ git2/ConsoleApplication1/Class.h
@@ -0,0 +1,21 @@
+#ifndef _HEADER_H_
+#define _HEADER_H_
+
+#include <iostream>
+using namespace std;
+
+class func
+{
+private:
+	float res, fir, sec;
+public:
+	void set(); 
+	void getf(); 
+	void gets(); 
+	float sum(); 
+	float sub(); 
+	float mul(); 
+	float div(); 
+};
+
+#endif // _HEADER_H_
